c253bbc4717282d807126128f414c3cc2850e775
gabrielesanticchi <gabriele.santicchi@soccerment.com>
2026-06-29 11:29:30 +0200

    day4 completed

---
+++ assets/sports_science_queries.py
@@ -0,0 +1,11 @@
+"""
+Sports science queries for the Qdrant certification hands-on.
+
+A list of queries to test the sports science search engine.
+"""
+
+queries = [
+    "GPS tracking accuracy in soccer",
+    "load monitoring and injury prevention",
+    "training intensity and performance",
+]
