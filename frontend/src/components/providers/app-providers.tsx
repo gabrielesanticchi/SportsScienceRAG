242675ebc0bfe48f53a7444ca0bef2bf4dbca510
gabrielesanticchi <gabriele.santicchi@soccerment.com>
2026-07-02 20:19:42 +0200

    chore: remove backend/frontend/handson, scaffold sportsscience_rag package

--- frontend/src/components/providers/app-providers.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { AuthProvider } from "@/components/providers/auth-provider";
-
-export function AppProviders({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
